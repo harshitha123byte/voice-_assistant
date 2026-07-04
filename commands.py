3dd0fa02a6e974320e6477b13f822db5976fdd05
Harshitha M <harshitham336@gmail.com>
2026-07-04 21:33:58 +0530

    Add functions to open Google, YouTube, calculator, and Notepad

---
+++ commands.py
@@ -0,0 +1,14 @@
+import webbrowser
+import os
+
+def open_google():
+    webbrowser.open("https://www.google.com")
+
+def open_youtube():
+    webbrowser.open("https://www.youtube.com")
+
+def open_calculator():
+    os.system("calc")
+
+def open_notepad():
+    os.system("notepad")
